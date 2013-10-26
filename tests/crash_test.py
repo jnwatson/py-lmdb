5ac451558f11fac0e19995131ced47d1acf0769e
David Wilson <dw@botanicus.net>
2013-10-26 15:33:52 +0100

    Move & tidy up crash_test.py.

--- tests/crash_test.py
+++ tests/crash_test.py
@@ -2,59 +2,29 @@
 # This is not a test suite! More like a collection of triggers for previously
 # observed crashes. Want to contribute to py-lmdb? Please write a test suite!
 #
+# what happens when empty keys/ values passed to various funcs
+# incorrect types
+# try to break cpython arg parsing - too many/few/incorrect args
 
-import operator
+import atexit
 import os
-import shutil
-import unittest
 import random
+import shutil
+import tempfile
+import unittest
 
 import lmdb
 
 
-DB_PATH = '/ram/dbtest'
-
-
-def make_asserter(op, ops):
-    def ass(x, y, msg='', *a):
-        if msg:
-            if a:
-                msg %= a
-            msg = ' (%s)' % msg
-
-        f = '%r %s %r%s'
-        assert op(x, y), f % (x, ops, y, msg)
-    return ass
-
-lt = make_asserter(operator.lt, '<')
-eq = make_asserter(operator.eq, '==')
-le = make_asserter(operator.le, '<=')
-
-def assertCrash(fn, *args, **kwargs):
-    try:
-        fn(*args, **kwargs)
-    except (TypeError, lmdb.Error):
-        return
-    assert 0, '%r(%r, %r) did not crash as expected' % (fn, args, kwargs)
-
-
-def rmenv():
-    if os.path.exists(DB_PATH):
-        shutil.rmtree(DB_PATH)
-
-def openenv(**kwargs):
-    return lmdb.open(DB_PATH, max_dbs=10, **kwargs)
-
-
 class EnvMixin:
     def setUp(self):
-        rmenv()
-        self.env = openenv(map_size=1048576*1024)
+        self.path = tempfile.mkdtemp(prefix='lmdb_test')
+        atexit.register(shutil.rmtree, self.path, ignore_errors=True)
+        self.env = lmdb.open(self.path, max_dbs=10)
 
     def tearDown(self):
         self.env.close()
         del self.env
-        shutil.rmtree(DB_PATH)
 
 
 class CrashTest(EnvMixin, unittest.TestCase):
@@ -73,7 +43,7 @@ class CrashTest(EnvMixin, unittest.TestCase):
     def testCloseWithTxn(self):
         txn = self.env.begin(write=True)
         self.env.close()
-        assertCrash(lambda: list(txn.cursor()))
+        self.assertRaises(Exception, (lambda: list(txn.cursor())))
 
     def testDoubleClose(self):
         self.env.close()
@@ -87,14 +57,14 @@ class CrashTest(EnvMixin, unittest.TestCase):
     def testTxnCloseActiveIter(self):
         with self.env.begin() as txn:
             it = txn.cursor().iternext()
-        assertCrash(lambda: list(it))
+        self.assertRaises(Exception, (lambda: list(it)))
 
     def testDbCloseActiveIter(self):
         db = self.env.open_db(name='dave3')
         with self.env.begin(write=True) as txn:
             txn.put('a', 'b', db=db)
             it = txn.cursor(db=db).iternext()
-        assertCrash(lambda: list(it))
+        self.assertRaises(Exception, (lambda: list(it)))
 
 
 class LeakTest(EnvMixin, unittest.TestCase):
@@ -123,76 +93,76 @@ class CursorTest(EnvMixin, unittest.TestCase):
         self.c = self.txn.cursor()
 
     def testKeyValueItemEmpty(self):
-        eq('', self.c.key())
-        eq('', self.c.value())
-        eq(('', ''), self.c.item())
+        self.assertEqual('', self.c.key())
+        self.assertEqual('', self.c.value())
+        self.assertEqual(('', ''), self.c.item())
 
     def testFirstLastEmpty(self):
-        eq(False, self.c.first())
-        eq(False, self.c.last())
+        self.assertEqual(False, self.c.first())
+        self.assertEqual(False, self.c.last())
 
     def testFirstFilled(self):
         putData(self.txn)
-        eq(True, self.c.first())
-        eq(ITEMS[0], self.c.item())
+        self.assertEqual(True, self.c.first())
+        self.assertEqual(ITEMS[0], self.c.item())
 
     def testLastFilled(self):
         putData(self.txn)
-        eq(True, self.c.last())
-        eq(ITEMS[-1], self.c.item())
+        self.assertEqual(True, self.c.last())
+        self.assertEqual(ITEMS[-1], self.c.item())
 
     def testSetKey(self):
-        assertCrash(lambda: self.c.set_key(''))
-        eq(False, self.c.set_key('missing'))
+        self.assertRaises(Exception, (lambda: self.c.set_key('')))
+        self.assertEqual(False, self.c.set_key('missing'))
         putData(self.txn)
-        eq(True, self.c.set_key('b'))
-        eq(False, self.c.set_key('ba'))
+        self.assertEqual(True, self.c.set_key('b'))
+        self.assertEqual(False, self.c.set_key('ba'))
 
     def testSetRange(self):
-        eq(False, self.c.set_range('x'))
+        self.assertEqual(False, self.c.set_range('x'))
         putData(self.txn)
-        eq(False, self.c.set_range('x'))
-        eq(True, self.c.set_range('a'))
-        eq('a', self.c.key())
-        eq(True, self.c.set_range('ba'))
-        eq('baa', self.c.key())
+        self.assertEqual(False, self.c.set_range('x'))
+        self.assertEqual(True, self.c.set_range('a'))
+        self.assertEqual('a', self.c.key())
+        self.assertEqual(True, self.c.set_range('ba'))
+        self.assertEqual('baa', self.c.key())
         self.c.set_range('')
-        eq('a', self.c.key())
+        self.assertEqual('a', self.c.key())
 
     def testDeleteEmpty(self):
-        eq(False, self.c.delete())
+        self.assertEqual(False, self.c.delete())
 
     def testDeleteFirst(self):
         putData(self.txn)
-        eq(False, self.c.delete())
+        self.assertEqual(False, self.c.delete())
         self.c.first()
-        eq(('a', ''), self.c.item())
-        eq(True, self.c.delete())
-        eq(('b', ''), self.c.item())
-        eq(True, self.c.delete())
-        eq(('baa', ''), self.c.item())
-        eq(True, self.c.delete())
-        eq(('d', ''), self.c.item())
-        eq(True, self.c.delete())
-        eq(('', ''), self.c.item())
-        eq(False, self.c.delete())
-        eq(('', ''), self.c.item())
+        self.assertEqual(('a', ''), self.c.item())
+        self.assertEqual(True, self.c.delete())
+        self.assertEqual(('b', ''), self.c.item())
+        self.assertEqual(True, self.c.delete())
+        self.assertEqual(('baa', ''), self.c.item())
+        self.assertEqual(True, self.c.delete())
+        self.assertEqual(('d', ''), self.c.item())
+        self.assertEqual(True, self.c.delete())
+        self.assertEqual(('', ''), self.c.item())
+        self.assertEqual(False, self.c.delete())
+        self.assertEqual(('', ''), self.c.item())
 
     def testDeleteLast(self):
         putData(self.txn)
-        eq(True, self.c.last())
-        eq(('d', ''), self.c.item())
-        eq(True, self.c.delete())
-        eq(('', ''), self.c.item())
-        eq(False, self.c.delete())
-        eq(('', ''), self.c.item())
+        self.assertEqual(True, self.c.last())
+        self.assertEqual(('d', ''), self.c.item())
+        self.assertEqual(True, self.c.delete())
+        self.assertEqual(('', ''), self.c.item())
+        self.assertEqual(False, self.c.delete())
+        self.assertEqual(('', ''), self.c.item())
 
     def testCount(self):
-        assertCrash(lambda: self.c.count())
+        self.assertRaises(Exception, (lambda: self.c.count()))
         putData(self.txn)
         self.c.first()
         # TODO: complete dup key support.
-        #eq(1, self.c.count())
+        #self.assertEqual(1, self.c.count())
 
     def testPut(self):
         pass
@@ -204,33 +174,33 @@ class IteratorTest(EnvMixin, unittest.TestCase):
         self.c = self.txn.cursor()
 
     def testEmpty(self):
-        eq([], list(self.c))
-        eq([], list(self.c.iternext()))
-        eq([], list(self.c.iterprev()))
+        self.assertEqual([], list(self.c))
+        self.assertEqual([], list(self.c.iternext()))
+        self.assertEqual([], list(self.c.iterprev()))
 
     def testFilled(self):
         putData(self.txn)
-        eq(ITEMS, list(self.c))
-        eq(ITEMS, list(self.c))
-        eq(ITEMS, list(self.c.iternext()))
-        eq(ITEMS[::-1], list(self.txn.cursor().iterprev()))
-        eq(ITEMS[::-1], list(self.c.iterprev()))
-        eq(ITEMS, list(self.c))
+        self.assertEqual(ITEMS, list(self.c))
+        self.assertEqual(ITEMS, list(self.c))
+        self.assertEqual(ITEMS, list(self.c.iternext()))
+        self.assertEqual(ITEMS[::-1], list(self.txn.cursor().iterprev()))
+        self.assertEqual(ITEMS[::-1], list(self.c.iterprev()))
+        self.assertEqual(ITEMS, list(self.c))
 
     def testFilledSkipForward(self):
         putData(self.txn)
         self.c.set_range('b')
-        eq(ITEMS[1:], list(self.c))
+        self.assertEqual(ITEMS[1:], list(self.c))
 
     def testFilledSkipReverse(self):
         putData(self.txn)
         self.c.set_range('b')
-        eq(REV_ITEMS[-2:], list(self.c.iterprev()))
+        self.assertEqual(REV_ITEMS[-2:], list(self.c.iterprev()))
 
     def testFilledSkipEof(self):
         putData(self.txn)
-        eq(False, self.c.set_range('z'))
-        eq(REV_ITEMS, list(self.c.iterprev()))
+        self.assertEqual(False, self.c.set_range('z'))
+        self.assertEqual(REV_ITEMS, list(self.c.iterprev()))
 
 
 
@@ -251,7 +221,6 @@ class MultiCursorDeleteTest(EnvMixin, unittest.TestCase):
         txn = self.env.begin(write=True)
         cur = txn.cursor()
         while cur.first():
-            print 'Deleting', cur.key()
             cur.delete()
 
         for i in xrange(1, 10):
@@ -268,7 +237,7 @@ class MultiCursorDeleteTest(EnvMixin, unittest.TestCase):
 
 
     def test_monster(self):
-        # Generate predictable sequence of sizes.
+        # Generate predictable sself.assertEqualuence of sizes.
         rand = random.Random()
         rand.seed(0)
 
@@ -282,19 +251,11 @@ class MultiCursorDeleteTest(EnvMixin, unittest.TestCase):
 
         deleted = 0
         for key in txn.cursor().iternext(values=False):
-            #print 'got key', key, 'deleting..'
-            print repr(key), deleted
             assert txn.delete(key), key
             deleted += 1
 
         assert deleted == len(keys), deleted
 
 
-
 if __name__ == '__main__':
     unittest.main()
-
-
-# what happens when empty keys/ values passed to various funcs
-# incorrect types
-# try to break cpython arg parsing - too many/few/incorrect args
