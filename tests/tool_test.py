f5e7cc6744611d517854b1015e3d202434ca4054
David Wilson <dw@botanicus.net>
2013-11-09 17:42:56 +0000

    Add tool_test to ensure Python3.x compat.

---
+++ tests/tool_test.py
@@ -0,0 +1,37 @@
+#
+# Copyright 2013 The py-lmdb authors, all rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted only as authorized by the OpenLDAP
+# Public License.
+#
+# A copy of this license is available in the file LICENSE in the
+# top-level directory of the distribution or, alternatively, at
+# <http://www.OpenLDAP.org/license.html>.
+#
+# OpenLDAP is a registered trademark of the OpenLDAP Foundation.
+#
+# Individual files and/or contributed packages may be copyright by
+# other parties and/or subject to additional restrictions.
+#
+# This work also contains materials derived from public sources.
+#
+# Additional information about OpenLDAP can be obtained at
+# <http://www.openldap.org/>.
+#
+
+from __future__ import absolute_import
+import unittest
+
+import lmdb
+import lmdb.tool
+
+
+class ToolTest(unittest.TestCase):
+    def test_ok(self):
+        # For now, simply ensure the module can be compiled (3.x compat).
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
