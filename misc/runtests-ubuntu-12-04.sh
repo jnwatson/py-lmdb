9ca092728e95e83afd332fcfef3f37b79b133fc9
David Wilson <dw@botanicus.net>
2015-06-03 02:41:04 -0400

    More Travis CI fixes.

--- misc/runtests-ubuntu-12-04.sh
+++ misc/runtests-ubuntu-12-04.sh
@@ -18,26 +18,26 @@ native() {
     clean
     quiet $1 setup.py develop
     quiet $1 -c 'import lmdb.cpython'
-    with_gdb $1 `type -p $2` tests || fail=1
+    with_gdb $1 -m pytest tests || fail=1
 }
 
 cffi() {
     clean
     LMDB_FORCE_CFFI=1 quiet $1 setup.py install
     LMDB_FORCE_CFFI=1 quiet $1 -c 'import lmdb.cffi'
-    with_gdb $1 `type -p $2` tests || fail=1
+    with_gdb $1 -m pytest tests || fail=1
 }
 
-native python2.5 py.test-2.5
-native python2.6 py.test-2.6
-native python2.7 py.test-2.7
-native python3.3 py.test-3.3
-cffi pypy "pypy -mpy.test"
-cffi python2.6 py.test-2.6
-cffi python2.7 py.test-2.7
-cffi python3.1 py.test-3.1
-cffi python3.2 py.test-3.2
-cffi python3.3 py.test-3.3
+native python2.5
+native python2.6
+native python2.7
+native python3.3
+cffi pypy
+cffi python2.6
+cffi python2.7
+cffi python3.1
+cffi python3.2
+cffi python3.3
 
 [ "$fail" ] && exit 1
 exit 0
