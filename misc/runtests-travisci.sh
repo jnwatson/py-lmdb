9a4b8d0e97ddbcc63b0845fb542766d36339b1fe
David Wilson <dw@botanicus.net>
2016-01-24 15:18:23 +0000

    More 6 week old Python release breakage.

--- misc/runtests-travisci.sh
+++ misc/runtests-travisci.sh
@@ -13,7 +13,7 @@ quiet add-apt-repository -y ppa:fkrull/deadsnakes
 quiet add-apt-repository -y ppa:pypy
 quiet apt-get -qq update
 quiet apt-get install --force-yes -qq \
-    python{2.5,2.6,2.7,3.2,3.3}-dev \
+    python{2.5,2.6,2.7,3.3}-dev \
     pypy-dev \
     libffi-dev \
     gdb
@@ -26,7 +26,7 @@ quiet python2.5 ez_setup_24.py
 quiet python2.6 ez_setup.py
 quiet python2.7 ez_setup.py
 # quiet python3.1 ez_setup.py
-quiet python3.2 ez_setup.py
+# quiet python3.2 ez_setup.py
 quiet python3.3 ez_setup.py
 quiet pypy ez_setup.py
 
@@ -34,7 +34,7 @@ quiet python2.5 -measy_install py==1.4.20 pytest==2.5.2
 quiet python2.6 -measy_install py==1.4.20 pytest==2.5.2 cffi
 quiet python2.7 -measy_install py==1.4.20 pytest==2.5.2 cffi
 # quiet python3.1 -measy_install py==1.4.20 pytest==2.5.2 cffi argparse
-quiet python3.2 -measy_install py==1.4.20 pytest==2.5.2 cffi
+# quiet python3.2 -measy_install py==1.4.20 pytest==2.5.2 cffi
 quiet python3.3 -measy_install py==1.4.20 pytest==2.5.2 cffi
 quiet pypy -measy_install py==1.4.20 pytest==2.5.2
 
