8bdbf8267b6d287b02d57da100a815510f8cae8a
Nic Watson <github@nicwatson.org>
2020-08-20 13:19:45 -0400

    Allow environment copy to take a passed-in transaction (#247)
    Add a patch that adds two new C lmdb APIs `mdb_env_copy{fd,}3` that take a transaction argument. The copy will have the contents of the database from the view of the transaction. This requires `compact=True`.
    
    Modify setup to apply the patch unless `LMDB_PURE` environment variable is set.
    
    Cleanup CI.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2013 The py-lmdb authors, all rights reserved.
+# Copyright 2013-2020 The py-lmdb authors, all rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted only as authorized by the OpenLDAP
@@ -25,6 +25,7 @@ from __future__ import with_statement
 
 import os
 import sys
+import shutil
 import platform
 
 from setuptools import Extension
@@ -63,21 +64,62 @@ else:
     extra_library_dirs = []
 
 extra_include_dirs += ['lib/py-lmdb']
+extra_compile_args = []
+
+patch_lmdb_source = False
 
 if os.getenv('LMDB_FORCE_SYSTEM') is not None:
     print('py-lmdb: Using system version of liblmdb.')
     extra_sources = []
     extra_include_dirs += []
     libraries = ['lmdb']
-else:
-    print('py-lmdb: Using bundled liblmdb; override with LMDB_FORCE_SYSTEM=1.')
+elif os.getenv('LMDB_PURE') is not None:
+    print('py-lmdb: Using bundled unmodified liblmdb; override with LMDB_FORCE_SYSTEM=1.')
     extra_sources = ['lib/mdb.c', 'lib/midl.c']
     extra_include_dirs += ['lib']
     libraries = []
+else:
+    print('py-lmdb: Using bundled liblmdb with py-lmdb patches; override with LMDB_FORCE_SYSTEM=1 or LMDB_PURE=1.')
+    extra_sources = ['build/lib/mdb.c', 'build/lib/midl.c']
+    extra_include_dirs += ['build/lib']
+    extra_compile_args += ['-DHAVE_PATCHED_LMDB=1']
+    libraries = []
+    patch_lmdb_source = True
 
+if patch_lmdb_source:
+    if sys.platform.startswith('win'):
+        try:
+            import patch_ng as patch
+        except ImportError:
+            raise Exception('Building py-lmdb from source on Windows requires the "patch-ng" python module.')
+
+    # Clean out any previously patched files
+    dest = 'build' + os.sep + 'lib'
+    try:
+        os.mkdir('build')
+    except Exception:
+        pass
+
+    try:
+        shutil.rmtree(dest)
+    except Exception:
+        pass
+    shutil.copytree('lib', dest)
+
+    # Copy away the lmdb source then patch it
+    if sys.platform.startswith('win'):
+        patchfile = 'lib' + os.sep + 'py-lmdb' + os.sep + 'env-copy-txn.patch'
+        patchset = patch.fromfile(patchfile)
+        rv = patchset.apply(2, root=dest)
+        if not rv:
+            raise Exception('Applying patch failed')
+    else:
+        rv = os.system('/usr/bin/patch -N -p3 -d build/lib < lib/py-lmdb/env-copy-txn.patch')
+        if rv:
+            raise Exception('Applying patch failed')
 
 # distutils perplexingly forces NDEBUG for package code!
-extra_compile_args = ['-UNDEBUG']
+extra_compile_args += ['-UNDEBUG']
 
 # Disable some Clang/GCC warnings.
 if not os.getenv('LMDB_MAINTAINER'):
@@ -91,7 +133,7 @@ if not os.getenv('LMDB_MAINTAINER'):
 # inttypes.h and stdint.h lack, and to avoid having to modify the LMDB source
 # code. Advapi32 is needed for LMDB's use of Windows security APIs.
 p = sys.version.find('MSC v.')
-msvc_ver = int(sys.version[p+6:p+10]) if p != -1 else None
+msvc_ver = int(sys.version[p + 6: p + 10]) if p != -1 else None
 
 if sys.platform.startswith('win'):
     # If running on Visual Studio<=2010 we must provide <stdint.h>. Newer
@@ -149,36 +191,34 @@ def grep_version():
                 return eval(line.split()[-1])
 
 setup(
-    name = 'lmdb',
-    version = grep_version(),
-    description = "Universal Python binding for the LMDB 'Lightning' Database",
-    long_description = "Universal Python binding for the LMDB 'Lightning' Database",
-    long_description_content_type = "text/plain",
-    author = 'David Wilson',
-    maintainer = 'Nic Watson',
-    license = 'OpenLDAP BSD',
-    url = 'http://github.com/jnwatson/py-lmdb/',
-    packages = ['lmdb'],
-    classifiers = [
+    name='lmdb',
+    version=grep_version(),
+    description="Universal Python binding for the LMDB 'Lightning' Database",
+    long_description="Universal Python binding for the LMDB 'Lightning' Database",
+    long_description_content_type="text/plain",
+    author='David Wilson',
+    maintainer='Nic Watson',
+    license='OpenLDAP BSD',
+    url='http://github.com/jnwatson/py-lmdb/',
+    packages=['lmdb'],
+
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.0",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Database",
         "Topic :: Database :: Database Engines/Servers",
     ],
-    ext_package = 'lmdb',
-    ext_modules = ext_modules,
-    install_requires = install_requires,
-    zip_safe = False
+    ext_package='lmdb',
+    ext_modules=ext_modules,
+    install_requires=install_requires,
 )
