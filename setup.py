4e5c2ebf47ff1da60c01254bfdcac116c893abfc
Nic Watson <github@nicwatson.org>
2025-10-14 23:12:44 -0400

    Bump version to 1.7.5 for release

--- setup.py
+++ setup.py
@@ -225,6 +225,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Database",
         "Topic :: Database :: Database Engines/Servers",
     ],
